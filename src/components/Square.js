2fec208976643fc689f85983ebf22ffe1cb3f3bb
nolastemgarden <nolastemgarden@gmail.com>
2020-06-28 13:51:41 -0500

    Create Square.js.  Super simple, extracted from ClassicGame.js.

---
+++ src/components/Square.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// My Styling
+// import '../App.css';
+
+// My Components
+import Circle from "./Circle";
+
+// MUI  components
+import Box from '@material-ui/core/Box';
+
+
+// Custom Styling
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    square: {
+        width: '100px',
+        height: '100px',
+        backgroundColor: theme.palette.primary.main,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+}));
+
+
+export default function Square(props) {
+    const classes = useStyles();
+    const status = props.status;
+
+    // console.log("Square passes status to Circle: " + props.status);    // Status may be 'red' 'yellow' or 'open'
+    return (
+        <Box
+            key={props.id}
+            className={classes.square}
+        >
+            <Circle status={status} />
+        </Box>
+    )
+}
+
