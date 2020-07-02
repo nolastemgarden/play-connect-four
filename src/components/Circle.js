aa63ca3d86fd9587c1f2f3a6c3f73454f88f28b9
nolastemgarden <nolastemgarden@gmail.com>
2020-07-02 08:45:43 -0500

    Delete Unused imports

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -5,14 +5,8 @@ import '../App.css';
 
 // My Components
 
-
 // MUI  components
 import Box from '@material-ui/core/Box';
-
-// Custom Styling
-import { makeStyles } from '@material-ui/core/styles';
-
-
 
 
 export default function Circle(props) {
