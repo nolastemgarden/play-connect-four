6554f6c957658dff354d2ebd2b0634093ba80072
nolastemgarden <nolastemgarden@gmail.com>
2020-07-05 17:21:15 -0500

    Updates

---
+++ precache-manifest.e8ab83d05de73bcbc2ac04405544d3e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9795a429106f559be87a9748b8e7212",
+    "url": "/play-connect-four/index.html"
+  },
+  {
+    "revision": "4d989d27e9ce0e2e08d6",
+    "url": "/play-connect-four/static/css/main.62bb5a8b.chunk.css"
+  },
+  {
+    "revision": "8bbd2483cc2a2af70d07",
+    "url": "/play-connect-four/static/js/2.010447ae.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/play-connect-four/static/js/2.010447ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d989d27e9ce0e2e08d6",
+    "url": "/play-connect-four/static/js/main.ecd4a851.chunk.js"
+  },
+  {
+    "revision": "802b3f3278548f5cfc24",
+    "url": "/play-connect-four/static/js/runtime-main.5f1da384.js"
+  }
+]);
