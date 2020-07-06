ea0f04073f6355e75a948c7d136df6ef53400480
nolastemgarden <nolastemgarden@gmail.com>
2020-07-06 15:15:07 -0500

    Rewrite Welcome Page and Modes of Play
    The welcome page text was summarized and converted from <expansionPanels> into regular typography...  The <ModesOfPlay> component was rewritten to use expansion pannels and to have the Play Now button as part of the expansion panel summary rather than in a collected ButtonArea below the panels.

--- src/components/ModesOfPlay.js
+++ src/components/ModesOfPlay.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+    Link as RouterLink,
+} from "react-router-dom";
 
 // My Components
 
@@ -11,7 +14,13 @@ import React from 'react';
 // import MenuItem from '@material-ui/core/MenuItem';
 // import MenuIcon from '@material-ui/icons/Menu';
 // import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography'
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 // import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 // import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -27,61 +36,156 @@ const useStyles = makeStyles((theme) => ({
     header: {
         padding: '16px',
     },
-    paragraph: {
-        paddingBottom: '8px',
-        paddingLeft: '16px',
+    
+    
+    panel: {
+        width: '100%',
+        padding: '0',
+        elevation: '0',
     },
+    summary: {
+        width: '100%',
+        padding: '0',
+        paddingRight: '0.7rem',
+        paddingLeft: '0.7rem',
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    summaryName: {
+        flexGrow: '1'
+    },
+    button: {
+        // border: 'solid red 1px',
+        margin: 'auto',
+        width: '18%',
+        
+    },
+    expansionDetail: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    paragraph: {
+        textIndent: '2rem',
+        paddingBottom: '1rem',
+    },
+    
 
 
 }));
 
+function ExpandPanelButton() {
+    const classes = useStyles();
+
+    return (
+        <Button
+            className={classes.button}
+            variant="outlined"
+            color="primary"
+            
+        >
+            <ExpandMoreIcon />
+        </Button>
+    )
+}
+
 
 export default function ModesOfPlay() {
     const classes = useStyles();
-    
+
+    const [expanded, setExpanded] = React.useState(false);
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+
 
     return (
         <React.Fragment>
 
-            <Typography variant='h5' className={classes.header} gutterBottom >
-                Classic Mode
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                Connect Four is a turn-based strategy game for two players.  The board is a grid with 6 rows and 7 columns.  Players take turns adding a chip of their color to the board and chips always ‘fall’ to the lowest empty row in the selected column.  Both players have the same goal: Get four chips of their color in a row.  Winning four-in-a-rows may be vertical, horizontal, or diagonal.
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                To make a move in the classic version, simply click on the column where you want to move.  If you are unfamiliar with the game, this is a good place to start.  Once you understand the mechanics of the game, move on to one of the versions where math fact practice is integrated.
-            </Typography>
+            <ExpansionPanel
+                className={classes.panel}
+                expanded={expanded === 'panel1'}
+                onChange={handleChange('panel1')}>
+                <ExpansionPanelSummary
+                    className={classes.summary}
+                    expandIcon={<ExpandPanelButton />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                >
+                    <Typography variant='h5' className={classes.summaryName} >
+                        Classic Mode
+                    </Typography>
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                        component={RouterLink}
+                        to='/classic_mode'
+                    >
+                        Play Now!
+                    </Button>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails className={classes.expansionDetail}>
+
+                    <Typography variant='body1' className={classes.paragraph} gutterBottom >
+                        Connect Four is a turn-based strategy game for two players.  The board is a grid with 6 rows
+                        and 7 columns.  Players take turns adding a chip of their color to a column of their choosing.
+                        Chips always ‘fall’ to the lowest empty square in the selected column.  Both players have the
+                        same goal: Get four adjacent chips in any row, column, or diagonal.
+                    </Typography>
+                    <Typography variant='body1' className={classes.paragraph} gutterBottom >
+                        To make a move in the classic version, simply click on the column where you want to move.
+                </Typography>
+
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
 
 
-            <Typography variant='h5' className={classes.header} gutterBottom >
-                Multiplication Practice Mode
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                In this version of the game, each row and column is assigned a number and each square on the board has a value that is the product of its row and column numbers.  Instead of clicking on the board to make a move, players must determine the product of the row and column where they wish to move and then enter that number in a text input field.
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                If the number entered corresponds to one of the squares that is currently the lowest empty square in its column, then the move is made successfully and it becomes the other player's turn.                        </Typography>
-            <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                If the number entered <em><strong>does not</strong></em> correspond to one of the squares that is currently the lowest empty square in its column, then no move is made and the player who entered an invalid number loses their turn.
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                By default, the game uses row and column numbers that are especially common in real world applications but you may use the game settings to choose numbers that your students especially need to practice with.
-            </Typography>
+            <ExpansionPanel
+                className={classes.panel}
+                expanded={expanded === 'panel2'}
+                onChange={handleChange('panel2')}>
+                <ExpansionPanelSummary
+                    className={classes.summary}
+                    expandIcon={<ExpandPanelButton />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                >
+                    <Typography variant='h5' className={classes.summaryName} >
+                        Multiplication Mode
+                    </Typography>
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                        component={RouterLink}
+                        to='/classic_mode'
+                    >
+                        Play Now!
+                    </Button>
+                </ExpansionPanelSummary>
+                
+                <ExpansionPanelDetails className={classes.expansionDetail}>
 
-            <Typography variant='h5' className={classes.header} gutterBottom >
-                Division Practice  Mode
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                This version has not been created yet.
-            </Typography>
+                    <Typography variant='body1' className={classes.paragraph} gutterBottom >
+                        In this version of the game, each row and column is assigned a number. 
+                        Instead of clicking on the board to make a move, players must determine the product of the row number and column number where they wish to move, 
+                        and then enter that number in a text input field.
+                    </Typography>
+                    <Typography variant='body1' className={classes.paragraph} gutterBottom >
+                        If the number entered <em><strong> does </strong></em> correspond to one of the squares that is currently the lowest empty square in its column, then the move is made successfully.
+                        If the number entered <em><strong> does not </strong></em> correspond to a legal move then then no move is made.
+                        Either way, it becomes the other player's turn.                        
+                    </Typography>
+                    <Typography variant='body1' className={classes.paragraph} gutterBottom >
+                        Teachers and Parents Note: By default, the game uses row and column numbers that are common in real world applications but you may use the game settings to choose numbers that your students need to practice with.
+                    </Typography>
 
-            <Typography variant='h5' className={classes.header} gutterBottom >
-                Rules of Exponents  Mode
-            </Typography>
-            <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                This version has not been created yet.
-            </Typography>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+
+            
 
         </React.Fragment>
     );
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-    Link as RouterLink,
-} from "react-router-dom";
+
 
 // My Components
 import Navbar from '../components/Navbar';
@@ -11,13 +9,7 @@ import ModesOfPlay from "../components/ModesOfPlay";
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box'; 
 import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography'
-
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
@@ -39,147 +31,71 @@ const useStyles = makeStyles((theme) => ({
         // backgroundColor: theme.palette.primary.light,
         backgroundColor: theme.palette.common.white,
         marginTop: '1.5rem',
-    },
-    header: {
         padding: '16px',
-    },
-    panel: {
-        width: '100%',
-    },
-    expansionDetail: {
-        display: 'flex',
-        flexDirection: 'column',
     },
     paragraph: {
         textIndent: '2rem',
-        paddingBottom: '1rem',
+        paddingBottom: '0.7rem',
     },
-    buttonArea: {
-        // border: 'solid red 1px',
-        margin: 'auto',
-        width: '70%',
-        padding: '16px',
-        display: 'flex',
-        justifyContent: 'space-around'
-    }
 
 }));
 
 
 export default function WelcomePage() {
     const classes = useStyles();
+    
     const [expanded, setExpanded] = React.useState(false);
-
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
     };
 
     
-    // What distinguishes Math from Science?
-    //    sci... Math...
-    //  Bottom Line: Proof vs evidence. 
-    // What does Tic Tac Toe have to do with this?
-    // --play now button
-    // What is the Fifteen Game?
+    
     
     return (
         <Container 
             className={classes.root} 
             maxWidth='md'
         >
-            <Navbar pageTitle={"Play Connect Four"} />
+            <Navbar pageTitle={"Welcome to Math Fact Connect Four"} />
 
             
             
             
             <Paper className={classes.paper} >
-                <Box className={classes.header}  >
-                    <Typography variant='h6' >
-                        {/* Welcome to my free version of the classic game Connect Four. <br /> */}
-                    </Typography>
-                    <Typography variant='h6' gutterBottom>
-                        Welcome to my free version of the classic game Connect Four. <br />
-                        My name is Nigel Wilson, and I am a math teacher with a passion for games where simple rules lead to complex strategies.
+                
+                <Typography variant='h5' gutterBottom>
+                    Welcome to my free version of the classic game Connect Four!                    
                 </Typography>
-                </Box>
+                <Typography variant='body1' className={classes.paragraph}>
+                    My name is Nigel Wilson, and I am a math teacher with a passion for games where simple rules lead to complex 
+                    strategies. I love to spice up math lessons by integrating them with such games.
+                </Typography>
+                <Typography variant='body1' className={classes.paragraph}>
+                    I developed the first version of this game in 2018 while I was working with an afterschool program in New Orleans 
+                    called College Track.  The first version of Math Fact Connect Four featured questions about the Rules of Exponents 
+                    printed on laminated sheets of paper and tacked to a wooden grid that hung at the front of the classroom. 
+                    The game was very popular with the students and they requested that I make more versions of it so that they 
+                    could use the connect four grid to practice more than just the rules of exponents.
+                </Typography>
+                <Typography variant='body1' className={classes.paragraph}>
+                    I was motivated by their enthusiastic response, but making the laminated cards was tedious, time consuming, and 
+                    imposed the limitation that the game could only be used by one class at a time.  Around the same time I was beginning to teach myself 
+                    web development, so I decided that it would be a better use of my time to make an online version of the game 
+                    rather than more laminated-card versions.  It would be good programming practice, I thought, and also would enable the game 
+                    to reach students beyond just my classes.   
+                </Typography>
+                <Typography variant='body1' className={classes.paragraph}>
+                    The game has a few versions that you can play in and all the code is open-source so if you want to help add 
+                    additional versions (or you are simply curious how it works under the hood!) you are welcome to &nbsp;
+                    <a href="https://github.com/nolastemgarden/play-connect-four">clone the repository</a>!
+                </Typography>
                 
-                {/* What Inspired Me to Create this Game? */}
-                <ExpansionPanel
-                    className={classes.panel}
-                    expanded={expanded === 'panel1'}
-                    onChange={handleChange('panel1')}>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1bh-content"
-                        id="panel1bh-header"
-                    >
-                        <Typography variant='h4'  >
-                            What inspired me to create this game?
-                        </Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails className={classes.expansionDetail}>
-                        
-                        <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                            In 2018, I was working as a math teacher with an afterschool program in New Orleans called College Track.  Our mission was to help kids from low-income households reach their goal of being first-generation college grads.  Many of the kids complained about the number of boring and tedious worksheets that they were expected to do to practice the math skills they would need on the ACT.  My objective in creating the first version of this game was to make practicing the rules of exponents more fun and engaging.  
-                        </Typography>
-                        <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                            I worked with the students to build a wooden grid that could be hung at the front of the classroom and a set of laminated squares of paper that each had a question printed on it.  After a lesson on how exponents work, students formed two teams and practiced what they had learned through a friendly competition.  The game was very popular and the students requested that I make more versions of it so that they could use the connect four grid to practice more than just the rules of exponents.                        
-                        </Typography>
-                        <Typography variant='body1' className={classes.paragraph} gutterBottom >
-                            Their enthusiasm pleased me, but making the laminated cards was very time consuming and imposed the limitation that the game could only be used by one class at a time.  I want the work that I do as a teacher to have the broadest impact possible, so making something that can only be used by one class at a time is not ideal.  Around that time, I was getting started teaching myself to program in my spare time and it dawned on me that making a Web-app version of this game would be good programming practice and also enable the game to be played by more students.  
-                        </Typography>
-                          
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-
-
-                {/* What are the different modes I can play in? */}
-                <ExpansionPanel
-                    className={classes.panel}
-                    expanded={expanded === 'panel3'}
-                    onChange={handleChange('panel3')}>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel3bh-content"
-                        id="panel3bh-header"
-                    >
-                        <Typography variant='h4'  >
-                            What are the different modes I can play in?
-                        </Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails className={classes.expansionDetail}>
-                        
-                        <ModesOfPlay />
-                        
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
+                <Typography variant='h5' gutterBottom >
+                    What are the different modes I can play in?
+                </Typography>
                 
-
-                <Box className={classes.buttonArea}>
-                    <Button 
-                        variant="contained" 
-                        color="primary" 
-                        component={RouterLink}
-                        to='/classic_mode'
-                    >
-                        Play Classic Mode!
-                    </Button>
-                    
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        component={RouterLink}
-                        to='//multiplication_mode'
-                    >
-                        Play with Multiplication Practice
-                    </Button>
-
-                </Box>
-                
-              
-                
-
-                
+                <ModesOfPlay />
                 
             </Paper>
         </Container>
