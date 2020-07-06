c5e1f91221b9d0b722caaefa831db5faa37bd442
nolastemgarden <nolastemgarden@gmail.com>
2020-07-06 17:58:15 -0500

    Delete functions that now live in GameWrapper

--- src/pages/ClassicGame.js
+++ src/pages/ClassicGame.js
@@ -48,6 +48,7 @@ export default function ClassicGame(props) {
     // Game Constants
     const squaresPerCol = props.squaresPerCol;
     const squaresPerRow = props.squaresPerRow;
+    
     let totalSquares = squaresPerCol * squaresPerRow;
 
     let linesPerCol = (squaresPerCol - 4 >= 0) ? (squaresPerCol - 3) : 0;
@@ -61,28 +62,6 @@ export default function ClassicGame(props) {
 
     const lineIdToSquareIdsMap = props.lineIdToSquareIdsMap;     // The linesToSquaresMap has one Key:Value pair for each lineId to a four-element array containing the squareIds that make up that line.
     const squareIdToLineIdsMap = props.squareIdToLineIdsMap;     // The squaresToLinesMap has one Key:Value pair mapping each squareId to an array of the Lines that include that squarre. In classic 6x7 game this is at least 3 and at most 13 lines.
-
-
-    // // Game Constants
-    // const squaresPerCol = 6;
-    // const squaresPerRow = 7;
-    // let totalSquares = squaresPerCol * squaresPerRow;
-
-    //     // A "Line" is defined as an Array of four squareIds that together constitute a win. 
-    //     // There are four 'types' of Line: 'vertical', 'horizontal', 'upslash', 'downslash'
-    //     // Each Line has a unique id. Id's are unique even across types, are consecutive, and 0-indexed . 
-
-    // let linesPerCol = (squaresPerCol - 4 >= 0) ? (squaresPerCol - 3) : 0;
-    // let linesPerRow = (squaresPerRow - 4 >= 0) ? (squaresPerRow - 3) : 0;
-    
-    // let numberOfVerticalLines = linesPerCol * squaresPerRow;
-    // let numberOfHorizontalLines = linesPerRow * squaresPerCol;
-    // let numberOfUpslashLines = linesPerCol * linesPerRow;
-    // let numberOfDownslashLines = linesPerCol * linesPerRow;
-    // let totalNumberOfLines = numberOfVerticalLines + numberOfHorizontalLines + numberOfUpslashLines + numberOfDownslashLines;
-    
-    // const lineIdToSquareIdsMap = getLineIdToSquareIdsMap();     // The linesToSquaresMap has one Key:Value pair for each lineId to a four-element array containing the squareIds that make up that line.
-    // const squareIdToLineIdsMap = getSquareIdToLineIdsMap();     // The squaresToLinesMap has one Key:Value pair mapping each squareId to an array of the Lines that include that squarre. In classic 6x7 game this is at least 3 and at most 13 lines.
 
     
     // STATE INITIALIZING and UPDATING helpers
@@ -111,35 +90,6 @@ export default function ClassicGame(props) {
         return (col * squaresPerCol + row);  
     }
     
-    
-    // FIRST level BOOLEAN LINE helpers             // Currently there is only a Square.js functional Component, however if I defined a Square Class I would think that I could turn these functions that take squareId as a parameter and turn them into something that 'reads better' like Square.isStartOfVerticalLine() written on the Square object so that it has built in access to the relevant squareId and can be used in a no-parameter fashion. ??? 
-    function isStartOfVerticalLine(squareId) {
-        const rowNumber = getRowBySquareId(squareId);
-        return (squaresPerCol - rowNumber >= 4);
-    }
-    function isEndOfVerticalLine(squareId) {
-        // To check if a square is the end of a verticalLine we need its rowNumber.
-        // rowNumber has 0-based indexing. If we are in the 3rd row or above then return TRUE.
-        const rowNumber = getRowBySquareId(squareId);
-        return (rowNumber >= 3);
-    }
-    function isStartOfHorizontalLine(squareId) {
-        // To check if a square is the start of a horizontalLine we need its colNumber and the width of each row (squaresPerRow).
-        // The Columns are rendered by the Board with 0-based indexing and the left-most column is column 'zero'.
-        // HorizontalLines are defined as "starting" with their left-most square. If there are 3 MORE squares in the row to the right of it then return TRUE.
-        // colNumber has 0-based indexing. If we are in the 0-th row then the minimum squaresPerCol that should return true is 4
-        const colNumber = getColBySquareId(squareId);
-        return (squaresPerRow - colNumber >= 4);
-    }
-    function isStartOfUpslashLine(squareId) {
-        // A square is the Start Of an Upslash Line IFF it is BOTH the "start" of a vertical line AND the "start" of a horizontal line.  
-        return (isStartOfVerticalLine(squareId) && isStartOfHorizontalLine(squareId));
-    }
-    function isStartOfDownslashLine(squareId) {
-        // A square is the Start Of an Downslash Line IFF it is BOTH the "end" of a vertical line AND the "start" of a horizontal line.  
-        return (isEndOfVerticalLine(squareId) && isStartOfHorizontalLine(squareId));
-    }
-
     
     // Board to Column Helpers    
     function getColumnStatus(colNumber, boardStatus) {
@@ -175,6 +125,8 @@ export default function ClassicGame(props) {
         // console.log(`Board Status: ${boardStatus}`)
         return boardStatus;
     }
+
+
     function getLineStatusMap(updatedMoveList) {
         let turnNumber = updatedMoveList.length;   // turnNumbers are 0-indexed! When it is turn 0 there is still one status object in the history
         if (turnNumber <= 0) {
@@ -215,6 +167,8 @@ export default function ClassicGame(props) {
         // printLinesToStatusMap(updatedLineStatusMap)
         return updatedLineStatusMap;
     }
+
+
     function getGameStatus(turnNumber, lineStatusMap) {
         // console.log(`GET GAME STATUS recieved lineStatusMap: `)
         // printLinesToStatusMap(lineStatusMap);
@@ -222,23 +176,18 @@ export default function ClassicGame(props) {
         
         // There  are FIVE posssible Game Status values.  THREE cases where game is over.
         if (playerOneWins(lineStatusMap)){
-            // console.log("Player One Wins!")
             return 'playerOneWins';
         }
         else if (playerTwoWins(lineStatusMap)) {
-            // console.log("Player Two Wins!")
             return 'playerTwoWins';
         }
         else if (gameDrawn(lineStatusMap)) {
-            // console.log("Game Over. Draw.")
             return 'gameDrawn';
         }
         else if (playerOneToMove(turnNumber) === true) {
-            // console.log("Player One to Move.")
             return 'playerOneToMove';
         }
         else if (playerOneToMove(turnNumber) === false) {
-            // console.log("Player Two to Move.")
             return 'playerTwoToMove';
         }
         else {
@@ -246,7 +195,7 @@ export default function ClassicGame(props) {
             return 1;
         }
     
-        // Internally uesd Helper Functions
+        // Internally used Helper Functions
         function playerOneWins(lineStatusMap) {
             let playerOnesMoveCounts = [];
             lineStatusMap.forEach((lineStatus, lineId) => {
@@ -287,7 +236,9 @@ export default function ClassicGame(props) {
     // CLICK HANDLERS
     function handleColumnClick(colNumber) {
         console.log(`handleColumnClick has been called with colNumber: ${colNumber} and currentTurnNumber: ${currentTurnNumber} `)
-        let currentTurnStatus = history[currentTurnNumber];
+        
+        let copyOfHistory = history.slice(0, currentTurnNumber + 1);
+        let currentTurnStatus = copyOfHistory[currentTurnNumber];
         let status = currentTurnStatus.gameStatus;
         console.log(`Game Status before Handling Click : ${status}`)
         // There are TWO reasons we might Return Early: 
@@ -331,11 +282,11 @@ export default function ClassicGame(props) {
         // This is where we Would find and make the Computer Move if in Play vs. Computer Mode
         return 0;
     }
+
     function handleUndoButtonClick() {
-        // const shortenedHistory = history.slice(0, history.length - 1)
-        // console.log(`handleUndoButtonClick() removed ${history[history.length - 1]} . New Shortened history: ${shortenedHistory}`);
-        // setHistory(shortenedHistory);
+        setCurrentTurnNumber(--currentTurnNumber);
     }
+
     function handleNewGameButtonClick() {
         setHistory([statusOnTurnZero]);
         setCurrentTurnNumber(0);
@@ -369,106 +320,7 @@ export default function ClassicGame(props) {
     }
 
 
-    // // MAP MAKING FUNCTIONS
-    // function getLineIdToSquareIdsMap() {
-    //     let completeMap = new Map();
-    //     let partialMaps = [verticalLineMap(), horizontalLineMap(), upslashLineMap(), downslashLineMap()]
-
-    //     partialMaps.forEach(partialMap => {
-    //         partialMap.forEach((squareIdList, lineId) => {
-    //             completeMap.set(lineId, squareIdList);
-    //         });
-    //     })
-    //     // CONFUSING point: For now I have just silenced the following console.log because it runs everytime handleColumnClick is called.  This means the map is being recreated from scratch each turn of the game unnecesarily. Perhaps I could solve this by moving the entire ClassicGame() inside a wrapper component that is strictly for holding complex CONSTANTS that ClassGame uses but only needs to compute once such as the Maps.
-    //     // console.log(`Generated Map of LineIds to the four SquareIds in each. There are ${completeMap.size} LineIds in the Map.`)
-    //     // completeMap.forEach(logMapElement);
-    //     // The following four HELPERS bear responsibility for corresponding the lists of squares to their correct final lineIds.  This is simple for vertical lines but requires consideartion of the startingId for the latter three. 
-    //     function verticalLineMap() {
-    //         let map = new Map();
-    //         // Vertical Lines are assigned Ids starting from Zero.
-    //         let currentLineId = 0;
-
-    //         for (let squareId = 0; squareId < totalSquares; squareId++) {
-    //             if (isStartOfVerticalLine(squareId)) {
-    //                 let first = squareId + 0;
-    //                 let second = squareId + 1;
-    //                 let third = squareId + 2;
-    //                 let fourth = squareId + 3;
-    //                 let squaresInLine = [first, second, third, fourth];
-    //                 map.set(currentLineId, squaresInLine);
-    //                 currentLineId++;
-    //             }
-    //         }
-    //         return map;
-    //     }
-    //     function horizontalLineMap() {
-    //         let map = new Map();
-    //         // Horizontal Lines are assigned Ids starting from numberOfVerticalLines.
-    //         let currentLineId = numberOfVerticalLines;
-    //         for (let squareId = 0; squareId < totalSquares; squareId++) {
-    //             if (isStartOfHorizontalLine(squareId)) {
-    //                 let first = squareId + (0 * squaresPerCol);
-    //                 let second = squareId + (1 * squaresPerCol);
-    //                 let third = squareId + (2 * squaresPerCol);
-    //                 let fourth = squareId + (3 * squaresPerCol);
-    //                 let squaresInLine = [first, second, third, fourth];
-    //                 map.set(currentLineId, squaresInLine);
-    //                 currentLineId++;
-    //             }
-    //         }
-    //         return map;
-    //     }
-    //     function upslashLineMap() {
-    //         let map = new Map();
-    //         // Upslash Lines are assigned Ids starting from numberOfVerticalLines + numberOfHorizontalLines.
-    //         let currentLineId = numberOfVerticalLines + numberOfHorizontalLines;
-    //         for (let squareId = 0; squareId < totalSquares; squareId++) {
-    //             if (isStartOfUpslashLine(squareId)) {
-    //                 let first = squareId + 0 * (squaresPerCol + 1);
-    //                 let second = squareId + 1 * (squaresPerCol + 1);
-    //                 let third = squareId + 2 * (squaresPerCol + 1);
-    //                 let fourth = squareId + 3 * (squaresPerCol + 1);
-    //                 let squaresInLine = [first, second, third, fourth];
-    //                 map.set(currentLineId, squaresInLine);
-    //                 currentLineId++;
-    //             }
-    //         }
-    //         return map;
-    //     }
-    //     function downslashLineMap() {
-    //         let map = new Map();
-    //         // Downslash Lines are assigned Ids starting from numberOfVerticalLines + numberOfHorizontalLines + numberOfUpslashLines.
-    //         let currentLineId = numberOfVerticalLines + numberOfHorizontalLines + numberOfUpslashLines;
-    //         for (let squareId = 0; squareId < totalSquares; squareId++) {
-    //             if (isStartOfDownslashLine(squareId)) {
-    //                 let first = squareId + 0 * (squaresPerCol - 1);
-    //                 let second = squareId + 1 * (squaresPerCol - 1);
-    //                 let third = squareId + 2 * (squaresPerCol - 1);
-    //                 let fourth = squareId + 3 * (squaresPerCol - 1);
-    //                 let squaresInLine = [first, second, third, fourth];
-    //                 map.set(currentLineId, squaresInLine);
-    //                 currentLineId++;
-    //             }
-    //         }
-    //         return map;
-    //     }
-    //     return completeMap;
-    // }
-    // function getSquareIdToLineIdsMap() {
-    //     let squaresToLinesMap = new Map();
-    //     for (let squareId = 0; squareId < totalSquares; squareId++) {
-    //         squaresToLinesMap.set(squareId, []);
-    //     }
-    //     lineIdToSquareIdsMap.forEach((squaresList, lineId) => {
-    //         squaresList.forEach(squareId => {
-    //             squaresToLinesMap.set(squareId, squaresToLinesMap.get(squareId).concat(lineId));
-    //         })
-    //     })
-    //     // CONFUSING point: For now I have just silenced the following console.log because it runs everytime handleColumnClick is called.  This means the map is being recreated from scratch each turn of the game unnecesarily. Perhaps I could solve this by moving the entire ClassicGame() inside a wrapper component that is strictly for holding complex CONSTANTS that ClassGame uses but only needs to compute once such as the Maps.
-    //     console.log(`Mapped each of the ${totalSquares} SquareIds to the set of all Lines that include it.`)
-    //     // squaresToLinesMap.forEach(logMapElement);
-    //     return squaresToLinesMap;
-    // }
+    
     function initialLineStatusMap() {
         let lineStatusMap = new Map();
         for (let lineId = 0; lineId < totalNumberOfLines; lineId++) {
@@ -491,7 +343,7 @@ export default function ClassicGame(props) {
             className={classes.root}
             maxWidth='md'
         >
-            <Navbar pageTitle={"Classic Connect Four"} />
+            
             <Paper className={classes.paper} >
                 
                 <Board 
@@ -502,8 +354,8 @@ export default function ClassicGame(props) {
                 />
 
                 <Panel 
-                    // panelStatus={getPanelStatus()}
-                    gameStatus="Status of Game"
+                    turnNumber={currentTurnNumber}
+                    gameStatus={history[currentTurnNumber].gameStatus}
                     handleUndoButtonClick={handleUndoButtonClick}
                     handleNewGameButtonClick={handleNewGameButtonClick}
                 />
